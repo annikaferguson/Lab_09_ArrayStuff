5bcba9b5930a2cb0878eaf124f26d5aa4aa6af9b
annikaferguson <fergusak@mail.uc.edu>
2023-07-11 09:29:23 -0400

    final question 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args)
+    {
+        Random rnd = new Random();
+        int[] dataPoints;
+
+        //1a
+        dataPoints = new int[100];
+
+        // 1b
+        for(int x = 0; x < dataPoints.length; x++)
+        {
+            dataPoints[x] = rnd.nextInt(100) + 1;
+        }
+
+        // 1c
+        for(int x = 0; x < dataPoints.length; x++)
+        {
+            System.out.printf("|%3d ", dataPoints[x]);
+        }
+
+        //1d sum and ave
+        int sum = 0;
+
+        for(int x = 0; x < dataPoints.length; x++)
+        {
+            sum = sum += dataPoints[x];
+        }
+        int ave = sum / dataPoints.length;
+
+        System.out.println("\nThe sum of the random array dataPoints is: " + sum);
+        System.out.println("The average of the random array dataPoints is: " + ave);
+
     }
 }
